259fdf29663b4779c8a7939c2086ffe020849020
kamal0620040 <neupane0620040@outlook.com>
2023-03-27 21:15:13 +0545

    Done basic setup for Lab 1

---
+++ Lab 1/Flag of Nepal/script.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector("#glcanvas");
+
+// Initialize the GL context
+const gl = canvas.getContext("webgl");
+
+// Only continue if WebGL is available and working
+if (gl === null) {
+  alert(
+    "Unable to initialize WebGL. Your browser or machine may not support it."
+  );
+}
