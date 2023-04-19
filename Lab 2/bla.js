9d1c1ea5adfd6fae0d712bc3a93e8c08d7e2f3a4
kamal0620040 <neupane0620040@outlook.com>
2023-04-19 21:43:53 +0545

    Fixed ISSUE of bla which is not able to plot when second endpoint is less than first endpoint

--- Lab 2/bla.js
+++ Lab 2/bla.js
@@ -1,16 +1,22 @@
 function BLALine(x1, y1, x2, y2){
     let dx = Math.abs(x2 - x1);
-    let dy = Math.abs(y2 - y1)
+    let dy = Math.abs(y2 - y1);
     let slope = dy/dx;
     
     let X, Y, steps, p;
     if(slope < 1){
+        if(x1 > x2){
+            // swap two endpoints
+            [temp1, temp2] = [x1, y1];
+            [x1, y1] = [x2, y2];
+            [x2, y2] = [temp1, temp2];
+        }
         X = x1;
         Y = y1;
         // TODO: Plot X, Y
         console.log(X, Y);
         p = 2 * dy - dx;
-        steps = x2 - x1;
+        steps = x1 < x2 ? x2 - x1 : x1 - x2;
         for (let i = 0; i < steps; i++){
             if(p < 0){
                 p = p + 2 * dy;
@@ -24,11 +30,18 @@ function BLALine(x1, y1, x2, y2){
         }
     }else{
         // for slope >= 1
+        if(y1 > y2){
+            // swap two endpoints
+            [temp1, temp2] = [x1, y1];
+            [x1, y1] = [x2, y2];
+            [x2, y2] = [temp1, temp2];
+        }
         X = x1;
         Y = y1;
         // TODO: Plot X, Y
         console.log(X, Y);
-        steps = y2 - y1;
+        steps = y1 < y2 ? y2 - y1 : y1 - y2;
+        // steps = y2 - y1;
         p = 2 * dx - dy;
         for(let i = 0; i < steps; i++){
             if(p < 0){
@@ -48,4 +61,6 @@ function BLALine(x1, y1, x2, y2){
 /* Considering my canvas of size 600 * 600 - [-600/600 = -1 ].
 ** So, point is plotted from (-1,-1) to (1,1) in clipspace
 */
- BLALine(-600, -600, 600, 600);
+//  BLALine(-600, -600, 600, 600);
+ BLALine(-100, -100, 100, 100);
+//  BLALine(100, 100, -100, -100);
